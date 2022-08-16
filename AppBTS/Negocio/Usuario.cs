df6df87fcdcf07c8dd47bc18de78ebb9970118a1
camilaGea <camilagea4@gmail.com>
2022-08-16 18:49:09 -0300

    prueba pav1

---
+++ AppBTS/Negocio/Usuario.cs
@@ -0,0 +1,46 @@
+﻿using AppBTS.Datos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBTS.Negocio
+{
+    internal class Usuario
+    {
+        private string nombre;
+        private string password;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        public Usuario()
+        {
+            nombre = password = string.Empty;
+        }
+        public Usuario(string nombre, string password)
+        {
+            this.nombre = nombre;
+            this.password = password;
+        }
+        public bool validar()
+        {
+            string consultaSQL = "SELECT * FROM Usuarios WHERE usuario='"
+                                 + this.Nombre + "' AND password='"
+                                 + this.Password + "'";
+            DBHelper oDB = new DBHelper();
+            if (oDB.consultarDB(consultaSQL).Rows.Count == 0)
+                return false;
+            else
+                return true;
+        }
+    }
+}
---
+++ AppBTS/Presentacion/frmPrincipal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppBTS
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+
+            frmLogin oFL = new frmLogin();
+            oFL.ShowDialog();
+            oFL.Dispose();
+        }
+    }
+}
